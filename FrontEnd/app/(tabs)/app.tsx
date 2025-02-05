54092a41e0f9bd9c75248abe74a680c1d314e386
Anusigan Sivananthan <148673260+Anusigan@users.noreply.github.com>
2025-02-05 14:47:05 +0530

    changes done in splash screen to display

--- FrontEnd/app/(tabs)/app.tsx
+++ FrontEnd/app/(tabs)/app.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import { Image, StyleSheet, Platform } from "react-native";
+
 import SplashScreen from "./splashscreen";
 import LoginOrSignUp from "./loginorsignup";
 const Stack = createStackNavigator();
 
-export default function App() {
+const App = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
@@ -16,4 +16,7 @@ export default function App() {
       </Stack.Navigator>
     </NavigationContainer>
   );
-}
+
+};
+export default App;
+
