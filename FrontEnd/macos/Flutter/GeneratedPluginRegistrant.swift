af91f738ee3d137207aa11adfffaa74deeeada92
Anusigan Sivananthan <148673260+Anusigan@users.noreply.github.com>
2025-02-20 11:37:47 +0530

    note screen updated

--- FrontEnd/macos/Flutter/GeneratedPluginRegistrant.swift
+++ FrontEnd/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,15 +6,17 @@ import FlutterMacOS
 import Foundation
 
 import audio_session
+import audioplayers_darwin
 import device_info_plus
 import just_audio
 import path_provider_foundation
-import record_macos
+import record_darwin
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AudioSessionPlugin.register(with: registry.registrar(forPlugin: "AudioSessionPlugin"))
+  AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   JustAudioPlugin.register(with: registry.registrar(forPlugin: "JustAudioPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
-  RecordMacosPlugin.register(with: registry.registrar(forPlugin: "RecordMacosPlugin"))
+  RecordPlugin.register(with: registry.registrar(forPlugin: "RecordPlugin"))
 }
