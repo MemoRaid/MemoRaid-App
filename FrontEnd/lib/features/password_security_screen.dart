84047a74337ca01e0265ebf6050be8899d18d29a
Anusigan Sivananthan <148673260+Anusigan@users.noreply.github.com>
2025-02-25 15:52:13 +0530

    updated the settings sub screens

--- FrontEnd/lib/features/password_security_screen.dart
+++ FrontEnd/lib/features/password_security_screen.dart
@@ -1,16 +1,252 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'user_provider.dart';
 
-class PasswordSecurityScreen extends StatelessWidget {
+class PasswordSecurityScreen extends StatefulWidget {
   const PasswordSecurityScreen({super.key});
 
   @override
+  _PasswordSecurityScreenState createState() => _PasswordSecurityScreenState();
+}
+
+class _PasswordSecurityScreenState extends State<PasswordSecurityScreen> {
+  final _formKey = GlobalKey<FormState>();
+  late String _currentPassword;
+  late String _newPassword;
+  late String _confirmPassword;
+
+  @override
+  void initState() {
+    super.initState();
+    final user = Provider.of<UserProvider>(context, listen: false).user;
+    _currentPassword = '';
+    _newPassword = '';
+    _confirmPassword = '';
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
-        title: Text('Password & Security'),
+        backgroundColor: Colors.white,
+        elevation: 0,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back, color: Color(0xFF0D3445)),
+          onPressed: () => Navigator.pop(context),
+        ),
+        title: Text(
+          "Password & Security",
+          style: TextStyle(
+            color: Color(0xFF0D3445),
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        centerTitle: true,
       ),
-      body: Center(
-        child: Text('Password & Security Screen'),
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Colors.white,
+              Color(0xFF0D3445),
+            ],
+          ),
+        ),
+        child: SingleChildScrollView(
+          child: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(height: 30),
+                _buildFormSection(),
+                SizedBox(height: 30),
+                _buildSaveButton(context),
+                SizedBox(height: 30),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildFormSection() {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          _buildSectionCard(
+            "Change Password",
+            [
+              _buildTextField(
+                labelText: 'Current Password',
+                prefixIcon: Icons.lock_outline,
+                obscureText: true,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your current password';
+                  }
+                  return null;
+                },
+                onSaved: (value) {
+                  _currentPassword = value ?? '';
+                },
+              ),
+              _buildTextField(
+                labelText: 'New Password',
+                prefixIcon: Icons.lock_outline,
+                obscureText: true,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter a new password';
+                  }
+                  return null;
+                },
+                onSaved: (value) {
+                  _newPassword = value ?? '';
+                },
+              ),
+              _buildTextField(
+                labelText: 'Confirm New Password',
+                prefixIcon: Icons.lock_outline,
+                obscureText: true,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please confirm your new password';
+                  }
+                  if (value != _newPassword) {
+                    return 'Passwords do not match';
+                  }
+                  return null;
+                },
+                onSaved: (value) {
+                  _confirmPassword = value ?? '';
+                },
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildSectionCard(String title, List<Widget> children) {
+    return Card(
+      elevation: 2,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(15),
+      ),
+      color: Color(0xFF0D3445), // Updated background color
+      child: Padding(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              title,
+              style: TextStyle(
+                color: Colors.white, // Updated text color for better contrast
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 16),
+            ...children
+                .expand((widget) => [widget, SizedBox(height: 16)])
+                .toList()
+              ..removeLast(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildTextField({
+    String? initialValue,
+    required String labelText,
+    String? hintText,
+    IconData? prefixIcon,
+    bool obscureText = false,
+    int maxLines = 1,
+    Function(String?)? onSaved,
+    String? Function(String?)? validator,
+  }) {
+    return TextFormField(
+      initialValue: initialValue,
+      maxLines: maxLines,
+      obscureText: obscureText,
+      style: TextStyle(color: Colors.white), // Updated text color
+      decoration: InputDecoration(
+        labelText: labelText,
+        labelStyle: TextStyle(color: Colors.white), // Updated label text color
+        hintText: hintText,
+        hintStyle: TextStyle(color: Colors.white70), // Updated hint text color
+        prefixIcon:
+            prefixIcon != null ? Icon(prefixIcon, color: Colors.white) : null,
+        contentPadding: EdgeInsets.symmetric(vertical: 12, horizontal: 16),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12),
+          borderSide: BorderSide(color: Colors.grey.shade300),
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12),
+          borderSide: BorderSide(color: Colors.grey.shade300),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12),
+          borderSide: BorderSide(color: Colors.white, width: 2),
+        ),
+        errorBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12),
+          borderSide: BorderSide(color: Colors.red.shade300),
+        ),
+        filled: true,
+        fillColor: Colors.grey.shade50.withOpacity(0.1),
+      ),
+      onSaved: onSaved,
+      validator: validator,
+    );
+  }
+
+  Widget _buildSaveButton(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+      child: ElevatedButton(
+        onPressed: () {
+          if (_formKey.currentState?.validate() ?? false) {
+            _formKey.currentState?.save();
+            // Update password logic here
+            Navigator.pop(context);
+          }
+        },
+        style: ElevatedButton.styleFrom(
+          backgroundColor: Color(0xFF4E6077),
+          foregroundColor: Colors.white,
+          padding: EdgeInsets.symmetric(vertical: 16),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12),
+          ),
+          elevation: 3,
+          shadowColor: Color(0xFF4E6077).withOpacity(0.5),
+          minimumSize: Size(double.infinity, 55),
+        ),
+        child: Text(
+          'Save Changes',
+          style: TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
       ),
     );
   }
