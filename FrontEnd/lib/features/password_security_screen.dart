e6ada261cf68f7fa9d5a1947c8b7ecc6da696fff
Anusigan Sivananthan <148673260+Anusigan@users.noreply.github.com>
2025-02-25 12:48:24 +0530

    sub parts for the settings screen are initialized

---
+++ FrontEnd/lib/features/password_security_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class PasswordSecurityScreen extends StatelessWidget {
+  const PasswordSecurityScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Password & Security'),
+      ),
+      body: Center(
+        child: Text('Password & Security Screen'),
+      ),
+    );
+  }
+}
