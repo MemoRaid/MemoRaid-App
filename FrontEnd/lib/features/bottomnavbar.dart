dffb403ecb719c90a559844d060ec5fe93c01571
Anusigan Sivananthan <148673260+Anusigan@users.noreply.github.com>
2025-02-26 14:52:48 +0530

    home screen graph updated

--- FrontEnd/lib/features/bottomnavbar.dart
+++ FrontEnd/lib/features/bottomnavbar.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 
 class CustomBottomNavigationBar extends StatelessWidget {
+  final int currentIndex;
   final Function(int) onTap;
-  const CustomBottomNavigationBar({super.key, required this.onTap});
+
+  const CustomBottomNavigationBar({
+    super.key,
+    required this.onTap,
+    this.currentIndex = 0,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -10,17 +16,36 @@ class CustomBottomNavigationBar extends StatelessWidget {
       backgroundColor: const Color(0xFF0D3445),
       selectedItemColor: Colors.white,
       unselectedItemColor: Colors.grey[400],
-      type: BottomNavigationBarType.fixed, // Prevent shifting animation
+      currentIndex: currentIndex,
+      type: BottomNavigationBarType.fixed,
       items: const [
         BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
         BottomNavigationBarItem(icon: Icon(Icons.bar_chart), label: "Progress"),
         BottomNavigationBarItem(
             icon: Icon(Icons.chat_bubble), label: "AI Chatbot"),
-        BottomNavigationBarItem(
-            icon: Icon(Icons.rocket_launch), label: "Rocket"),
+        BottomNavigationBarItem(icon: Icon(Icons.settings), label: "Settings"),
         BottomNavigationBarItem(icon: Icon(Icons.star), label: "Achievements"),
       ],
-      onTap: onTap,
+      onTap: (index) {
+        switch (index) {
+          case 0:
+            Navigator.pushReplacementNamed(context, '/home');
+            break;
+          case 1:
+            Navigator.pushReplacementNamed(context, '/progress');
+            break;
+          case 2:
+            Navigator.pushReplacementNamed(context, '/chatbot');
+            break;
+          case 3:
+            Navigator.pushReplacementNamed(context, '/settings');
+            break;
+          case 4:
+            Navigator.pushReplacementNamed(context, '/achievements');
+            break;
+        }
+        onTap(index);
+      },
     );
   }
 }
