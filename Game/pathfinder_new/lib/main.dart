b5ce545fb261501f7274a04c310b63decea1b698
Rishaanth15 <rrishaanth@gmail.com>
2025-03-04 21:46:28 +0530

    Adding main entry point

--- Game/pathfinder_new/lib/main.dart
+++ Game/pathfinder_new/lib/main.dart
@@ -1,1 +1,22 @@
-// main.dart file
+import 'package:flutter/material.dart';
+import 'start_screen.dart';
+
+void main() {
+  runApp(const PathFinderApp());
+}
+
+class PathFinderApp extends StatelessWidget {
+  const PathFinderApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Path Finder',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: const StartScreen(),
+    );
+  }
+}
