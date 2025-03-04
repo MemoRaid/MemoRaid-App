fa8e12a3092ca78ca300d2957d890d46d10005b1
Rishaanth15 <rrishaanth@gmail.com>
2025-03-04 22:03:32 +0530

    Added animation utilities

--- Game/pathfinder_new/lib/main.dart
+++ Game/pathfinder_new/lib/main.dart
@@ -168,3 +168,16 @@ class Dot {
   }
 }
 
+import 'package:flutter/material.dart';
+
+class Animations {
+  static Widget fadeInWidget(Widget child) {
+    return TweenAnimationBuilder(
+      duration: const Duration(seconds: 1),
+      tween: Tween<double>(begin: 0, end: 1),
+      builder: (context, value, child) => Opacity(opacity: value, child: child),
+      child: child,
+    );
+  }
+}
+
