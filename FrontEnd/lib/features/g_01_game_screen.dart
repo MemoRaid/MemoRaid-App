f8400b6f6f238bd7eacc6df5aa5603336fd5ff41
Linushankaran18 <linushankaranjana@gmail.com>
2025-03-14 11:32:41 +0530

    initializeGame

--- FrontEnd/lib/features/g_01_game_screen.dart
+++ FrontEnd/lib/features/g_01_game_screen.dart
@@ -27,4 +27,55 @@ class _GameScreenState extends State<GameScreen> {
     super.initState();
     _initializeGame();
   }
+
+  void _initializeGame() {
+    scenarios = [
+      Scenario(
+        id: 1,
+        title: "Morning Routine",
+        description: "Arrange the steps for getting ready in the morning",
+        steps: [
+          "Wake up and get out of bed",
+          "Brush teeth and wash face",
+          "Take a shower",
+          "Get dressed",
+          "Eat breakfast",
+          "Pack bag for the day",
+          "Leave the house"
+        ],
+      ),
+      Scenario(
+        id: 2,
+        title: "Making Pasta",
+        description: "Put these steps in order to make pasta",
+        steps: [
+          "Fill pot with water",
+          "Bring water to a boil",
+          "Add salt to water",
+          "Add pasta to boiling water",
+          "Cook pasta until al dente",
+          "Drain pasta",
+          "Add sauce and serve"
+        ],
+      ),
+      Scenario(
+        id: 3,
+        title: "Grocery Shopping",
+        description: "Arrange the steps for a successful grocery trip",
+        steps: [
+          "Make a shopping list",
+          "Grab reusable bags",
+          "Drive to the store",
+          "Select items and put in cart",
+          "Wait in checkout line",
+          "Pay for groceries",
+          "Load groceries in car and drive home"
+        ],
+      ),
+    ];
+
+    currentScenarioIndex = 0;
+    score = 0;
+    _resetScenario();
+  }
 }
