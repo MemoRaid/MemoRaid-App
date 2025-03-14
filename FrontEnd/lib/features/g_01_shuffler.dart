da3ed0f31ad83f46ed62dddad1cc2c6d07624259
Anusigan Sivananthan <148673260+Anusigan@users.noreply.github.com>
2025-03-14 12:34:29 +0530

    changes

--- FrontEnd/lib/features/g_01_shuffler.dart
+++ FrontEnd/lib/features/g_01_shuffler.dart
@@ -5,13 +5,12 @@ class Shuffler {
   static List<TaskStep> ShuffleSteps(List<TaskStep> steps) {
     final random = Random();
     final List<TaskStep> shuffled = List.from(steps);
+    for (int i = shuffled.length - 1; i > 0; i--) {
+      final int n = random.nextInt(i + 1);
+      final TaskStep temp = shuffled[i];
+      shuffled[i] = shuffled[n];
+      shuffled[n] = temp;
+    }
+    return shuffled;
   }
-
-  for (int i = shuffled.length - 1; i > 0; i--) {
-    final int n = random.nextInt(i + 1);
-    final TaskStep temp = shuffled[i];
-    shuffled[i] = shuffled[n];
-    shuffled[n] = temp;
-  }
-  return shuffled;
-} 
+}
