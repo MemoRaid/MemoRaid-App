980abb27276ef1803fe5b631a6f80b74e801bec0
Linushankaran18 <linushankaranjana@gmail.com>
2025-03-14 11:56:50 +0530

    shuffler class created

--- FrontEnd/lib/features/g_01_shuffler.dart
+++ FrontEnd/lib/features/g_01_shuffler.dart
@@ -1,2 +1,9 @@
 import '../features/g_01_task_step.dart';
 import 'dart:math';
+
+class Shuffler {
+  static List<TaskStep> ShuffleSteps(List<TaskStep> steps) {
+    final random = Random();
+    final List<TaskStep> shuffled = List.from(steps);
+  }
+}
